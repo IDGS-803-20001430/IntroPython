be894b15e068d26101c1b285c9ba4c0cb302abb5
IDGS-803-20001430 <lrerick20001430@gmail.com>
2024-01-12 18:48:06 -0600

    EALR 12/01/2023 Tuplas

---
+++ tema1-Variables.py
@@ -0,0 +1,29 @@
+#NOMBRE DE VARUABLES
+
+'''
+comentarios
+multi
+lineas
+'''
+
+numero1 = "23"
+
+numero2 = "10"
+
+nombre="Domingo"
+
+correo="domingo@gmail.com"
+
+print(numero1+numero2)
+
+#CONVERSION FORZOSA EBTRE TIPOS DE DATOS
+print("La suma es: ")
+
+print(int(numero1)+int(numero2))
+
+sueldo = float("936.9")
+print(sueldo)
+
+edad=283
+
+print(len(str(edad)))
